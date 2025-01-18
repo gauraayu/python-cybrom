9f0cd6b8a6c75e4992738f6c6299859036ec37ea
gauraayu <gauraayushi2004@gmail.com>
2025-01-18 19:00:49 +0530

    first commit

---
+++ my_str.py
@@ -0,0 +1,24 @@
+import string
+# special character
+spe_ctr =string.punctuation
+
+print(spe_ctr)
+print(len(spe_ctr))
+
+
+x=string.ascii_letters
+z=string.ascii_lowercase
+y=string.ascii_uppercase
+p=string.hexdigits
+q=string.octdigits
+r=string.digits
+s=string.printable
+
+print(x)
+print(z)
+print(y)
+print(p)
+print(q)
+print(r)
+print(s)
+print(len(s))
