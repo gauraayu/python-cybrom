d73f5a52c77b1ecc3c780354a356ebdfe3b594f3
gauraayu <gauraayushi2004@gmail.com>
2025-01-24 18:54:36 +0530

    update

--- indexing.py
+++ indexing.py
@@ -5,4 +5,4 @@ print(str1.index('O'))
 # print(str1.index('t'))  will show error
 # print(str1.index('T',3)) #this will start searching after 3rd index and thid will show error
 # print(str1.index('obj',start,stop))
-print(str1.index('T',0,4))   #0 is start point #4 is the end point for searching
+print(str1.index('T',0,4))   #0 is start point #4 is the end point for searching....        
---
+++ slice_method.py
@@ -0,0 +1,10 @@
+s1='python'
+print(s1[:])
+# 1. step ka directn nhi dia toh by default +ve 
+# 2.  +ve directn
+# 3.  o/p=python
+s2='python'
+print(s1[::]) 
+# 1. step directn=+ve
+# 2.  +ve directn
+#  3.o/p= python
