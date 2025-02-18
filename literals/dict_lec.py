103479f2d45aa22e732a758b0cb72056fa941a56
gauraayu <gauraayushi2004@gmail.com>
2025-02-18 11:21:20 +0530

    update

--- literals/dict_lec.py
+++ literals/dict_lec.py
@@ -3,5 +3,6 @@ print(d1)
 print(type(d1))
 d2={'name':'aayu','age':25 ,'qualification':'M.tech'}
 print(d2)
+# to find id of both together
 print(id(d1),id(d2))
 # dic m value nikalne k lie key ka use krte h 
